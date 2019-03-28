5587ff71f06af6667d0ec26cc39bbe121969b9eb
seungwon park <yyyyy@snu.ac.kr>
2019-03-29 00:19:47 +0900

    cleanup inspection codes

--- inspection.py
+++ inspection.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # # Comparison of Inverse-STFT implementations
 # - Seungwon Park's implementation: IFFT + deconvolution for stacking `ytmp`
 # - Keunwoo Choi's implementation: based on IRFFT
@@ -26,8 +23,10 @@ def timing(func):
         time = start.elapsed_time(end)
         return output,time
     return inner
+
 istft_irfft = timing(istft_irfft)
 istft_deconv = timing(istft_deconv)
+
 def test_stft():
     import traceback
     audio, sr = librosa.load(librosa.util.example_audio_file(), duration=2,sr=None)
@@ -60,55 +59,6 @@ def test_stft():
 #                print(traceback.print_exc())
 
 
-
-
-
-
-
 if __name__ == "__main__":
     test_stft()
-=======
-# In[3]:
-
-
-y, sr = librosa.load(librosa.util.example_audio_file(), duration=2.0)
-n_fft = 2048
-hop_length = n_fft // 4
-y = torch.tensor(y)
-stft = torch.stft(y, n_fft, hop_length)
-
-
-# In[4]:
-
-
-stft_single = stft
-stft_batch = stft.unsqueeze(0)
-
-
-# In[5]:
-
-
-result_deconv = istft_deconv(stft_single, hop_length)
-result_irfft = istft_irfft(stft_batch, hop_length)[0]
-
-diff = torch.max(torch.abs(result_deconv - result_irfft)).item()
-
-if diff < 1e-4:
-        print(f'Results are consistent. Maximum difference: {diff}')
-
-
-# In[6]:
-
-
-get_ipython().run_line_magic('timeit', 'result_deconv = istft_deconv(stft_single, hop_length)')
-
-
-# In[7]:
-
-
-get_ipython().run_line_magic('timeit', 'result_irfft = istft_irfft(stft_batch, hop_length)[0]')
-
-
-# # Conclusion
-# - IRFFT-based implementation is faster, showing that better parallelization doesn't outspeed algorithmic optimization.
 
